82e1193232b66c5f9e4c02fa25c9ced23702d179
TR4Q <jlchin@berkeley.edu>
2018-01-19 22:34:11 -0800

    Adds skeleton for json schema validation.

---
+++ nba_dashboard/test/validate_api_response.py
@@ -0,0 +1,20 @@
+import jsonschema
+
+EXAMPLE_PLAYER_IDS = []
+EXAMPLE_GAME_IDS = []
+EXAMPLE_GAME_DATES = []
+
+def validate_player_profiles():
+    pass
+
+def validate_player_logs():
+    pass
+
+def validate_player_averages():
+    pass
+
+def validate_game():
+    pass
+
+def validate_game_date_games():
+    pass
