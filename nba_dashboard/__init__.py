f073c6e2b2b956c52b0290d332184bb7ad5ffab0
Jason Chin <jlchin@berkeley.edu>
2018-01-20 12:22:25 -0800

    Adds react skeleton. (#4)
    * Adds node/react/js to .gitignore.
    
    * Adds react skeleton and updates flask accordingly.
    
    * Updates README with build instructions.

--- nba_dashboard/__init__.py
+++ nba_dashboard/__init__.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, request, g, redirect, url_for, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static/dist",
+            template_folder="static")
 
 app.config.from_envvar('DASHBOARD_CONFIG')
---
+++ nba_dashboard/static/js/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return <p> Hello React!</p>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("content"));
