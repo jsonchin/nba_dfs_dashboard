53a6e7924ff10132c0762873b812db30699feee1
TR4Q <jlchin@berkeley.edu>
2018-01-21 15:12:49 -0800

    Adds app to dashboard entry.

--- nba_dashboard/dashboard.py
+++ nba_dashboard/dashboard.py
@@ -1,3 +1,4 @@
+from .import app
 from .api_endpoints import player_profile_endpoint, player_logs_endpoint, player_averages_endpoint
 from .api_endpoints import game_endpoint, game_team_specific_endpoint, game_date_games_endpoint
 from .views import show_index
